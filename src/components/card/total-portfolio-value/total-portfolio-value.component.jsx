52ef6ae7c132a5fc7a7df4374aed6c67985b14fb
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2024-09-24 09:03:23 +0530

    Add total portfolio value card

---
+++ src/components/card/total-portfolio-value/total-portfolio-value.component.jsx
@@ -0,0 +1,12 @@
+import "./total-portfolio-value.style.css";
+
+const TotalPortfolioValue = () => {
+  return (
+    <div className="total-portfolio-value-main">
+      <p>Total Portfolio Value</p>
+      <p>0.00</p>
+    </div>
+  );
+};
+
+export default TotalPortfolioValue;
